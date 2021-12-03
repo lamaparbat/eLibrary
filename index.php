a2d23b50bb54f713cf776aa400cc4a9c82565144
Parbat <parbatlama70@gmail.com>
2021-12-03 23:28:17 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ <link rel="stylesheet" href="./css/navbar.css">
+ <link rel="stylesheet" href="./css/index.css">
+ <title>Library Management System</title>
+</head>
+
+<body>
+ <!-- header -->
+ <?php include 'navbar.php' ?>
+
+ <!-- body -->
+ <br /><span id="title">Control Panel</span>
+ <div class="main mt-3">
+
+  <!-- row1 -->
+  <div class="box1">
+   <div class="d-flex justify-content-between">
+    <span>Books</span>
+    <i class="fa fa-ellipsis-h mt-1" aria-hidden="true"></i>
+   </div>
+   <div class="body pt-3">
+    <span>1201</span><br />
+    <h6>Total Books available</h6>
+    <p>From 2018 - 2020</p>
+   </div>
+   <div class="footer d-flex justify-content-end">
+    <div>
+     <i class="fa fa-folder-open" aria-hidden="true"></i>
+    </div>
+   </div>
+  </div>
+
+  <div class="box2">
+   <div class="d-flex justify-content-between">
+    <span>Books</span>
+    <i class="fa fa-ellipsis-h mt-1" aria-hidden="true"></i>
+   </div>
+   <div class="body pt-3">
+    <span>1201</span><br />
+    <h6>Total Books available</h6>
+    <p>From 2018 - 2020</p>
+   </div>
+   <div class="footer d-flex justify-content-end">
+    <div>
+     <i class="fa fa-folder-open" aria-hidden="true"></i>
+    </div>
+   </div>
+  </div>
+
+  <div class="box3">
+   <div class="d-flex justify-content-between">
+    <span>Books</span>
+    <i class="fa fa-ellipsis-h mt-1" aria-hidden="true"></i>
+   </div>
+   <div class="body pt-3">
+    <span>1201</span><br />
+    <h6>Total Books available</h6>
+    <p>From 2018 - 2020</p>
+   </div>
+   <div class="footer d-flex justify-content-end">
+    <div>
+     <i class="fa fa-folder-open" aria-hidden="true"></i>
+    </div>
+   </div>
+  </div>
+
+  <div class="box4">
+   <div class="d-flex justify-content-between">
+    <span>Books</span>
+    <i class="fa fa-ellipsis-h mt-1" aria-hidden="true"></i>
+   </div>
+   <div class="body pt-3">
+    <span>1201</span><br />
+    <h6>Total Books available</h6>
+    <p>From 2018 - 2020</p>
+   </div>
+   <div class="footer d-flex justify-content-end">
+    <div>
+     <i class="fa fa-folder-open" aria-hidden="true"></i>
+    </div>
+   </div>
+  </div>
+
+  <!-- row2 -->
+  <div class="box1">
+   <div class="d-flex justify-content-between">
+    <span>Books</span>
+    <i class="fa fa-ellipsis-h mt-1" aria-hidden="true"></i>
+   </div>
+   <div class="body pt-3">
+    <span>1201</span><br />
+    <h6>Total Books available</h6>
+    <p>From 2018 - 2020</p>
+   </div>
+   <div class="footer d-flex justify-content-end">
+    <div>
+     <i class="fa fa-folder-open" aria-hidden="true"></i>
+    </div>
+   </div>
+  </div>
+
+  <div class="box2">
+   <div class="d-flex justify-content-between">
+    <span>Books</span>
+    <i class="fa fa-ellipsis-h mt-1" aria-hidden="true"></i>
+   </div>
+   <div class="body pt-3">
+    <span>1201</span><br />
+    <h6>Total Books available</h6>
+    <p>From 2018 - 2020</p>
+   </div>
+   <div class="footer d-flex justify-content-end">
+    <div>
+     <i class="fa fa-folder-open" aria-hidden="true"></i>
+    </div>
+   </div>
+  </div>
+
+  <div class="box3">
+   <div class="d-flex justify-content-between">
+    <span>Books</span>
+    <i class="fa fa-ellipsis-h mt-1" aria-hidden="true"></i>
+   </div>
+   <div class="body pt-3">
+    <span>1201</span><br />
+    <h6>Total Books available</h6>
+    <p>From 2018 - 2020</p>
+   </div>
+   <div class="footer d-flex justify-content-end">
+    <div>
+     <i class="fa fa-folder-open" aria-hidden="true"></i>
+    </div>
+   </div>
+  </div>
+
+  <div class="box4">
+   <div class="d-flex justify-content-between">
+    <span>Books</span>
+    <i class="fa fa-ellipsis-h mt-1" aria-hidden="true"></i>
+   </div>
+   <div class="body pt-3">
+    <span>1201</span><br />
+    <h6>Total Books available</h6>
+    <p>From 2018 - 2020</p>
+   </div>
+   <div class="footer d-flex justify-content-end">
+    <div>
+     <i class="fa fa-folder-open" aria-hidden="true"></i>
+    </div>
+   </div>
+  </div>
+
+ </div>
+
+
+ <!-- footer -->
+
+
+ <!-- bootstrap mdn -->
+ <script src="./js/navbar.js"></script>
+ <script src="https://use.fontawesome.com/8af7dff76b.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,30 @@
+<div id="mySidenav" class="sidenav">
+ <div id="avatar" class="mt-4 mb-2">
+  <img />
+ </div>
+ <span>Parbat Lama</span>
+ <hr />
+ <a href="javascript:void(0)" class="closebtn bg-white" onclick="closeNav()">&times;</a>
+ <a href="#"> <i class="fa fa-home" aria-hidden="true"></i> Dashboard</a>
+ <a href="#"> <i class="fa fa-users" aria-hidden="true"></i> Members</a>
+ <a href="#"> <i class="fa fa-book" aria-hidden="true"></i> Books</a>
+ <a href="#"> <i class="fa fa-rocket" aria-hidden="true"></i> Issued</a>
+ <a href="#"> <i class="fa fa-undo" aria-hidden="true"></i> Returned</a>
+ <a href="#"> <i class="fa fa-exclamation-circle" aria-hidden="true"></i> Not Returned</a>
+ <a href="#"> <i class="fa fa-cogs" aria-hidden="true"></i> Setting</a>
+</div>
+
+<!-- Use any element to open the sidenav -->
+<div id="navbar">
+ <div id="rightNav">
+  <i onclick="openNav()" class="fa fa-bars" aria-hidden="true"></i>
+  <img src="./img/logo.png" />
+ </div>
+ <div id="leftNav">
+  <i class="fa fa-plus" aria-hidden="true"></i>
+  <i class="fa fa-th" aria-hidden="true"></i>
+  <div id="avatar">
+   <img />
+  </div>
+ </div>
+</div>
