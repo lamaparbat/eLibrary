4790ea2159d5af06337ac439ebe7f8a8e24a744a
Parbat Lama <lamaparbat70@gmail.com>
2022-03-26 14:46:39 +0545

    Issued book card ui maintained..

---
+++ backend/getIssues.php
@@ -0,0 +1,43 @@
+<?php
+include 'connection.php';
+
+if (json_decode($_COOKIE["user_data"])[3] != "admin") {
+   header("Location: http://localhost/eLibrary/admin_login.php");
+}
+
+$query = "SELECT * FROM issued";
+$res = mysqli_query($con, $query) or die(mysqli_error($con));
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+   while ($row = mysqli_fetch_assoc($res)) {
+      //data
+      $id = $row["id"];
+      $username = $row["username"];
+      $user_email = $row["user_email"];
+      $bookname = $row["bookname"];
+      $book_id = $row["book_id"];
+      $issue_date = $row["issued_date"];
+      $deadline = $row["deadline"];
+      $src = 'backend/books_img/' . $row["src"];
+
+      echo '
+             <div class="box pb-2 mx-1 my-2">
+      <br />
+      <div class="bookImg">
+       <img src="' . $src . '" id="memberImg" loading="lazy" />
+      </div>
+      <div class="body d-flex flex-column">
+       <h5 class="mx-3 my-2"><b>' . $bookname . '</b></h5>
+       <span class="mx-3">User: <font id="value">' . $username . '</font></span>
+       <span class="mx-3">Took on: <font id="value">' . $issue_date . '</font></span>
+       <span class="mx-3">Deadline: <font class="bg-danger text-light" id="value">' . $deadline . '</font></span>
+      </div>
+      <div class="d-flex justify-content-around footer">
+       <div class="edit_cont px-2"><i class="fa fa-pencil" aria-hidden="true"></i> Returned</div>
+       <div class="edit_cont px-2"><i class="fa fa-trash" aria-hidden="true"></i></i> Delete</div>
+      </div><br/>
+     </div>
+
+      ';
+   }
+}
--- backend/issueBook.php
+++ backend/issueBook.php
@@ -4,7 +4,8 @@ include 'connection.php';
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  $book_id = $_POST["book_id"];
- $email = json_decode($_COOKIE["user_data"])[0];
+ $user_email = json_decode($_COOKIE["user_data"])[0];
+ $username = json_decode($_COOKIE["user_data"])[1];
  $issue_date = explode("/", date("Y/m/d"));
  $deadline = $issue_date[0] . "/" . ($issue_date[1] + 1) . "/" . $issue_date[2];
  $issue_date = $issue_date[0] . "/" . $issue_date[1] . "/" . $issue_date[2];
@@ -13,12 +14,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
  $query = "SELECT * FROM books WHERE id=$book_id";
  $result = mysqli_query($con, $query) or die(mysqli_error($con));
   while ($row = mysqli_fetch_assoc($result)) {
-    $user_email = json_decode($_COOKIE["user_data"])[0];
     $book_name = $row["name"];
     $src = $row["src"];
     
     //insert query
-    $query = "INSERT INTO issued(user_email, bookname, book_id, issued_date, deadline,src) VALUES('$user_email','$book_name','$book_id','$issue_date','$deadline','$src')";
+    $query = "INSERT INTO issued(username, user_email, bookname, book_id, issued_date, deadline,src) VALUES('$username','$user_email','$book_name','$book_id','$issue_date','$deadline','$src')";
     if(mysqli_query($con, $query) or die(mysqli_error($con))){
         echo "success";
     }else{
